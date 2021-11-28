048cfc99bf00287cf469347fc8754e2d0f93d7d3
Elena Radchenko <elena.radchenko.9@mail.ru>
2021-11-28 23:54:43 +0300

    added some changes to file dz

---
+++ Elena_Radchenko_dz_4/lesson_4_dz_3.py
@@ -0,0 +1,20 @@
+import requests
+import datetime
+
+
+def currency_rates(arg, date=None):
+    user_charcode = arg.upper()
+    url_list = requests.get("http://www.cbr.ru/scripts/XML_daily.asp").text.split("><")
+    char_val = {}
+    for el in url_list:
+        if el.find("CharCode") != -1:
+            charcode = el[9:12]
+        elif el.find("Value") != -1:
+            valute_value = float(el[6:13].replace(",", "."))
+            char_val[charcode] = valute_value
+    print(char_val.get(user_charcode, None), datetime.date.today())
+
+
+currency_rates("hgk")
+currency_rates("usd")
+currency_rates("eur")
