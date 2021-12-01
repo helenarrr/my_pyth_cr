9ef63c47ed62659b5aa0e33f879b550069e8da3e
Elena Radchenko <elena.radchenko.9@mail.ru>
2021-12-01 22:52:38 +0300

    added some changes to file

---
+++ Elena_Radchenko_dz_5/lesson_5_dz_1.py
@@ -0,0 +1,15 @@
+
+
+def nums_generator(n):
+   for num in range(1, n + 1, 2):
+       yield num
+
+
+nums_gen = nums_generator(10)
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
